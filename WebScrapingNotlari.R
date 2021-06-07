432529028bf2cf4e31f6b616250fe4302215ae44
Ahmet Akgül <rpydaneogrendim@gmail.com>
2021-06-07 13:21:28 +0300

    Update WebScrapingNotlari.R

--- WebScrapingNotlari.R
+++ WebScrapingNotlari.R
@@ -1,40 +1,40 @@
 ### Tavsiyeler ###
 
-#HTML&CSS bilgisi (web kazımadan daha yüksek verim almak için)
+#HTML&CSS bilgisi (web kazÄ±madan daha yÃ¼ksek verim almak iÃ§in)
 #HTML: https://www.w3schools.com/html/default.asp
 #CSS: https://www.w3schools.com/css/default.asp
 
-#rvest kütüphanesine ait dokümantasyon
+#rvest kÃ¼tÃ¼phanesine ait dokÃ¼mantasyon
 #https://cran.r-project.org/web/packages/rvest/rvest.pdf
 #read_html(), html_nodes(), html_text(), html_attr()
 
-#Veri düzenleme ile ilgili kütüphalere ait fonksiyonlar hakkında temel bilgiler
-#Özellikle string ifadeler ile nasıl çalışır? REGEX bilgisi!
+#Veri dÃ¼zenleme ile ilgili kÃ¼tÃ¼phalere ait fonksiyonlar hakkÄ±nda temel bilgiler
+#Ã–zellikle string ifadeler ile nasÄ±l Ã§alÄ±ÅŸÄ±r? REGEX bilgisi!
 #str_trim(), word(), str_match()
 #mutate(), rename(), na.omit()
 #gsub(), grepl(), seq(), as.numeric(), paste0(), print(), Sys.sleep(), as.data.frame()
 
-#for() döngüsü, tryCatch(), ifelse() nasıl çalışır?
+#for() dÃ¶ngÃ¼sÃ¼, tryCatch(), ifelse() nasÄ±l Ã§alÄ±ÅŸÄ±r?
 
-### Kütüphaneler ###
+### KÃ¼tÃ¼phaneler ###
 
-library(rvest) #Web kazıma kütüphanelerinden bir tanesidir
-library(tidyverse) #Veri düzenleme (dplyr, stringi, stringr gibi paketleri içerir)
-library(lubridate) #Tarihsel düzenleme paketlerinden bir tanesidir
+library(rvest) #Web kazÄ±ma kÃ¼tÃ¼phanelerinden bir tanesidir
+library(tidyverse) #Veri dÃ¼zenleme (dplyr, stringi, stringr gibi paketleri iÃ§erir)
+library(lubridate) #Tarihsel dÃ¼zenleme paketlerinden bir tanesidir
 library(openxlsx) #Excel olarak kaydet
 library(readxl) #Excel'i import et
 
-### Basit bir web kazıma örneği ###
+### Basit bir web kazÄ±ma Ã¶rneÄŸi ###
 
 #Hedef url
 url <- "https://www.hepsiemlak.com/buca-satilik/daire"
 
-#Evin fiyatı
+#Evin fiyatÄ±
 fiyat <- read_html(url) %>% 
   html_nodes("div.list-view-price") %>% #div'e ait bir class
   html_text()
 
-#İlan tarihi
+#Ä°lan tarihi
 tarih <- read_html(url) %>% 
   html_nodes("div.list-view-date") %>% #div'e ait bir class
   html_text()
@@ -44,19 +44,19 @@ m2 <- read_html(url) %>%
   html_nodes("span.celly.squareMeter") %>% #span'e ait bir class
   html_text()
 
-#Apartmanın yaşı
+#ApartmanÄ±n yaÅŸÄ±
 yas <- read_html(url) %>% 
   html_nodes("span.celly.buildingAge") %>% #span'e ait bir class
   html_text()
 
-#Bulunduğu konum
+#BulunduÄŸu konum
 lokasyon <- read_html(url) %>% 
   html_nodes("div.list-view-location") %>% #div'e ait bir class
   html_text()
 
-### Alınan bilgiler ile veri çerçevesi (dataframe) oluşturma ###
+### AlÄ±nan bilgiler ile veri Ã§erÃ§evesi (dataframe) oluÅŸturma ###
 
-df <- data.frame( #col_ ile sütun olduğunu belirt (şart değil özelleştirilebilir)
+df <- data.frame( #col_ ile sÃ¼tun olduÄŸunu belirt (ÅŸart deÄŸil Ã¶zelleÅŸtirilebilir)
   col_fiyat = fiyat,
   col_tarih = tarih,
   col_m2 = m2,
@@ -64,52 +64,52 @@ df <- data.frame( #col_ ile sütun olduğunu belirt (şart değil özelleştirilebilir
   col_lokasyon = lokasyon
 ) %>% 
   mutate(
-    #TL, boşluklar ve noktaları kaldır; numeric formata çevir
+    #TL, boÅŸluklar ve noktalarÄ± kaldÄ±r; numeric formata Ã§evir
     col_fiyat = as.numeric(gsub("\\.","",str_trim(gsub(" TL","",col_fiyat), side = "both"))),
-    #ymd tarih formatı
+    #ymd tarih formatÄ±
     col_tarih = dmy(col_tarih),
-    #m2 ve boşlukları kaldır; numeric formata çevir
+    #m2 ve boÅŸluklarÄ± kaldÄ±r; numeric formata Ã§evir
     col_m2 = as.numeric(str_trim(gsub(" m2","",col_m2), side = "both")),
-    #string ifadeleri kaldır; numeric'e çevir
-    col_yas = as.numeric(ifelse(grepl("Yaşında",col_yas), word(col_yas,1,1),
-                         ifelse(grepl("Sıfır", col_yas), 0, col_yas))),
-    #string ve boşlukları kaldır
+    #string ifadeleri kaldÄ±r; numeric'e Ã§evir
+    col_yas = as.numeric(ifelse(grepl("YaÅŸÄ±nda",col_yas), word(col_yas,1,1),
+                         ifelse(grepl("SÄ±fÄ±r", col_yas), 0, col_yas))),
+    #string ve boÅŸluklarÄ± kaldÄ±r
     col_lokasyon = str_trim(gsub("Buca,","",col_lokasyon), side = "both")
   )
 
-### For döngüsü ile web kazıma ###
+### For dÃ¶ngÃ¼sÃ¼ ile web kazÄ±ma ###
 
-#Birden fazla sayfayı kazıma
+#Birden fazla sayfayÄ± kazÄ±ma
 
-#İlk 3 sayfayı al
-#İstenilen sayıda girilebilir
+#Ä°lk 3 sayfayÄ± al
+#Ä°stenilen sayÄ±da girilebilir
 url2 <- str_c(
   "https://www.hepsiemlak.com/buca-satilik?page=",
   seq(1,3,1)
 )
 
-#url'lerin toplanacağı veri çerçevesi
+#url'lerin toplanacaÄŸÄ± veri Ã§erÃ§evesi
 urldf <- data.frame()
 
 for(i in 1:length(url2)){
   
-  #Her döngüde bir tbl oluştur
+  #Her dÃ¶ngÃ¼de bir tbl oluÅŸtur
   tbl <- read_html(url2[i]) %>% 
     html_nodes("div.links a") %>% 
     html_attr("href") %>% 
     as.data.frame() %>% 
     rename("colurl"=1)
   
-  #Oluşturulan tbl'i urldf veri çerçevesi ile birleştir
-  #Döngü her çalıştığında üzerinde kaydedecek
+  #OluÅŸturulan tbl'i urldf veri Ã§erÃ§evesi ile birleÅŸtir
+  #DÃ¶ngÃ¼ her Ã§alÄ±ÅŸtÄ±ÄŸÄ±nda Ã¼zerinde kaydedecek
   urldf <- urldf %>% bind_rows(tbl)
   
 }
 
-#Tam url oluştur; yani, "https://www.hepsiemlak.com" ile birleştir
+#Tam url oluÅŸtur; yani, "https://www.hepsiemlak.com" ile birleÅŸtir
 urldf$colurl <- paste0("https://www.hepsiemlak.com",urldf$colurl)
 
-#fiyat ve detay adında iki başlık aç; bilgiler buraya gelecek
+#fiyat ve detay adÄ±nda iki baÅŸlÄ±k aÃ§; bilgiler buraya gelecek
 masterdf <- data.frame(matrix("", nrow = nrow(urldf), ncol = 2)) %>% 
   rename(
     "fiyat" = 1,
@@ -118,21 +118,21 @@ masterdf <- data.frame(matrix("", nrow = nrow(urldf), ncol = 2)) %>%
 
 for(i in 1:nrow(urldf)){
   
-  #FALSE yaz. Nedeni devamında anlatılıyor.
+  #FALSE yaz. Nedeni devamÄ±nda anlatÄ±lÄ±yor.
   siradakine_gec <- FALSE
   
-  tryCatch( #tryCatch() hataya rağmen devam ettiren bir fonksiyondur
+  tryCatch( #tryCatch() hataya raÄŸmen devam ettiren bir fonksiyondur
     
-    expr = { #kodlar expr içinde yer alır
+    expr = { #kodlar expr iÃ§inde yer alÄ±r
       
-      #Döngü her çalıştığında ilgili url'i okuyacak (HTML)
+      #DÃ¶ngÃ¼ her Ã§alÄ±ÅŸtÄ±ÄŸÄ±nda ilgili url'i okuyacak (HTML)
       icerik <- read_html(as.character(urldf$colurl[i]))
       
       #Fiyat bilgisi
       masterdf[i,1] <- icerik %>% 
         html_nodes("div.right p") %>% 
         html_text()
-      #Tüm bilgilerin yer aldığı detay
+      #TÃ¼m bilgilerin yer aldÄ±ÄŸÄ± detay
       masterdf[i,2] <- icerik %>% 
         html_nodes("div.det-adv-info") %>% 
         html_text()
@@ -148,25 +148,25 @@ for(i in 1:nrow(urldf)){
     
   )
   
-  if(siradakine_gec){ #Eğer siradakine_gec TRUE olursa next ile döngü devam edecek
+  if(siradakine_gec){ #EÄŸer siradakine_gec TRUE olursa next ile dÃ¶ngÃ¼ devam edecek
     
     next
     
   }
   
-  Sys.sleep(time = 3) #İstekleri 3 saniyede bir gönder
-  write.xlsx(masterdf, "buca.xlsx") #Her döngüde excel'e kaydet
-  print(paste0(i,". işlem bitti...")) #Her döngüde bilgi ver
+  Sys.sleep(time = 3) #Ä°stekleri 3 saniyede bir gÃ¶nder
+  write.xlsx(masterdf, "buca.xlsx") #Her dÃ¶ngÃ¼de excel'e kaydet
+  print(paste0(i,". iÅŸlem bitti...")) #Her dÃ¶ngÃ¼de bilgi ver
   
 }
 
-buca <- read_excel("buca.xlsx") #Oluşturulan dosyayı import et
+buca <- read_excel("buca.xlsx") #OluÅŸturulan dosyayÄ± import et
 
-### Import edilen veri çerçevesinin düzenlenmesi ###
+### Import edilen veri Ã§erÃ§evesinin dÃ¼zenlenmesi ###
 
 buca_daire <- buca %>% 
-  na.omit() %>% #NA değerleri kaldır
+  na.omit() %>% #NA deÄŸerleri kaldÄ±r
   mutate(
-    #Ör: Isınma tipi verisinin elde edilmesi ya da iki string arasındaki ifadeyi alma
-    isinma_tipi = str_match(buca_daire$detay, "Isınma Tipi\\s*(.*?)\\s*Kat Sayısı")[,2]
+    #Ã–r: IsÄ±nma tipi verisinin elde edilmesi ya da iki string arasÄ±ndaki ifadeyi alma
+    isinma_tipi = str_match(detay, "IsÄ±nma Tipi\\s*(.*?)\\s*Kat SayÄ±sÄ±")[,2]
   ) #REGEX!
